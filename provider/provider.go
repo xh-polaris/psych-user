562bb1c2bb8fdafa8f0a4beee1372e0251fa56f5
remoolB <chengzixian1201@gmail.com>
2025-05-22 16:29:56 +0800

    feat：搭好了文件夹结构

---
+++ provider/provider.go
@@ -0,0 +1,38 @@
+package provider
+
+import (
+	"github.com/xh-polaris/psych-user/biz/infrastructure·/config"
+)
+
+var provider *Provider
+
+func Init() {
+	var err error
+	provider, err = NewProvider()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Provider 提供controller依赖的对象
+type Provider struct {
+	Config          *config.Config
+
+func Get() *Provider {
+	return provider
+}
+
+var RpcSet = wire.NewSet(
+)
+
+var ApplicationSet = wire.NewSet(
+)
+
+var InfrastructureSet = wire.NewSet(
+	config.NewConfig,
+)
+
+var AllProvider = wire.NewSet(
+	ApplicationSet,
+	InfrastructureSet,
+)

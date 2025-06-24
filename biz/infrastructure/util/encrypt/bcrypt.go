5f1e6693541be62f0f80f8aa249579b4d2d20d30
Zephyrtoria <2594379136@qq.com>
2025-06-24 22:33:37 +0800

    feat:

--- biz/infrastructure/util/encrypt/bcrypt.go
+++ biz/infrastructure/util/encrypt/bcrypt.go
@@ -1,8 +1,25 @@
 package encrypt
 
 import (
+	"github.com/xh-polaris/psych-user/biz/infrastructure/consts"
 	"golang.org/x/crypto/bcrypt"
+	"sync"
 )
+
+var defaultPwd string
+var once sync.Once
+
+func GetDefaultPwd() string {
+	once.Do(func() {
+		var pwd string
+		var err error
+		if pwd, err = BcryptEncrypt(consts.DefaultPassword); err != nil {
+			panic(err)
+		}
+		defaultPwd = pwd
+	})
+	return defaultPwd
+}
 
 // BcryptEncrypt Bcrypt加密函数
 func BcryptEncrypt(password string) (string, error) {
